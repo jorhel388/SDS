8502e62f09e7feb897ccebab5d0f60b250c9a793
jorhel388 <josemiguel.mr@gmail.com>
2025-07-05 20:19:38 -0400

    Primer commit

---
+++ plugins_registry.js
@@ -0,0 +1,11 @@
+function getPluginRegistry() {
+// last item should NOT have a comma 
+return {
+//#Auto-Inject-Start#
+
+//#Auto-Inject-End#
+//"Plugin Menu Name" : "Plugin Folder Name",
+//"Plugin Menu Name" : "Plugin Folder Name",
+//"Plugin Menu Name" : "Plugin Folder Name"
+};
+}
---
+++ plugins_sresults.js
@@ -0,0 +1,6 @@
+﻿
+// sets plugin result links to open in full window
+$("head").append("<base target='_parent' />");
+
+// links to plugins stylesheet in Studio help
+$("head").append("<link href='../../../Help/Content/plugins.css' rel='stylesheet' type='text/css'>");
